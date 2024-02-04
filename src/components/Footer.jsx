fc0a90a69d6e9d462a695e41eaeae5677d31bf69
‘Taichi-Sakai’ <130420550+sakai-taichi@users.noreply.github.com>
2024-02-04 22:30:14 +0900

    各種コンポーネント追加、head情報修正

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer>
+      <p>© TAICHI PHOTO</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/Main.jsx
@@ -0,0 +1,15 @@
+const Main = () => {
+  return (
+    <main>
+      <div className="Kv">
+        <p>KVです</p>
+      </div>
+
+      <div className="mainArticles">
+        <p>コンテンツです</p>
+      </div>
+    </main>
+  );
+}
+
+export default Main;
