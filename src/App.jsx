5040bfe6acafe0594672ea80857e4c573f7834cd
‘Taichi-Sakai’ <130420550+sakai-taichi@users.noreply.github.com>
2024-02-05 22:23:32 +0900

    bootstrapをインストール

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from './components/Header'
 import Main from './components/Main'
 import Footer from './components/Footer'
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'; // BootstrapのCSSをインポート
 
 function App() {
 
