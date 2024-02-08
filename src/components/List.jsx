5906df4b3f4e24491299873c0ba46c87f0e8b24b
‘Taichi-Sakai’ <130420550+sakai-taichi@users.noreply.github.com>
2024-02-09 01:46:44 +0900

    Listコンポーネント追加

---
+++ src/components/List.jsx
@@ -0,0 +1,28 @@
+import Card from 'react-bootstrap/Card';
+
+const List = (props) => {
+  console.log(props.src);
+
+  return (
+    <div className="mainArticles">
+      {
+        props.src.map((prop) => (
+          <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src={prop} />
+          <Card.Body>
+            <Card.Text>
+              テキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
+            </Card.Text>
+            <Card.Title>Card Title</Card.Title>
+          </Card.Body>
+        </Card>
+        ))
+      }
+    </div>
+  );
+}
+
+
+
+
+export default List;
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,13 +1,27 @@
 import Kv from "./Kv";
 import kvImg from "../images/KV_pc.jpeg";
+import List from "./List";
+import Img01 from "../images/image01.jpg";
+import Img02 from "../images/image02.jpeg";
+import Img03 from "../images/image03.jpeg";
+
+const photoList = [Img01,Img02,Img03];
+
+console.log(photoList);
 
 const Main = () => {
   return (
     <main>
       <Kv src={kvImg}/>
-      <div className="mainArticles">
-      <p>コンテンツです</p>
-      </div>
+      <ul>
+      {
+        photoList.map((photo) => (
+        <li key={photo} >
+        <List src={photo}/>
+        </li>
+        ))
+      }
+      </ul>
     </main>
   );
 }
