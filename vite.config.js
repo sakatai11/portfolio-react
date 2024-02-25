1d71011d172f72b08e50b672c6f6804a62e47c31
‘Taichi-Sakai’ <130420550+sakai-taichi@users.noreply.github.com>
2024-02-26 00:05:29 +0900

    pages配下の追加

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        list: resolve(__dirname, 'pages/list/index.html'),
+      },
+    },
+  },
 })
+
