f28375a284d090452f3a1b5836e35d04dbc2bf13
‘Taichi-Sakai’ <130420550+sakai-taichi@users.noreply.github.com>
2024-03-05 20:13:07 +0900

    マージ箇所修正

--- src/pages/list/App.jsx
+++ src/pages/list/App.jsx
@@ -12,9 +12,7 @@ function App() {
 
   // コンポーネントがマウントされた後に、opacityを1に変更する副作用フックを定義
   useEffect(() => { // ページの読み込みが完了したら実行されるイベントリスナーを追加
-    window.onload = () => {
       setOpacity('l-container display');
-    };
   }, []);
 
   const pageUrl = window.location.pathname;
