ef6ba611d9459dfe80f66f139b0b829c431f02e9
‘Taichi-Sakai’ <130420550+sakai-taichi@users.noreply.github.com>
2024-04-12 13:36:42 +0900

    レイアウト調整

--- src/components/PicMain.jsx
+++ src/components/PicMain.jsx
@@ -1,9 +1,10 @@
 import PicPhoto from "./PicPhoto";
+import styles from './layouts/ListMain.module.css'
 
 const PicMain = ( {id} ) => {
 
   return (
-    <main>
+    <main className={styles.listMain}>
       <div className="mainArticles">
       <PicPhoto id={id} />
       </div>
--- src/components/PicPhotoList.jsx
+++ src/components/PicPhotoList.jsx
@@ -1,21 +1,24 @@
+import styles from "./layouts/PicArea.module.css";
 
-const PicPhotoList = ({img}) => {
+const PicPhotoList = ( {img} ) => {
   console.log(img);
   console.log(img.image_list);
 
   return (
-    <div>
+    <>
       <h1>{img.camara}<span>{img.film}</span></h1>
-      <ul>
-        {
-          img.image_list.map((imageItem, index) => (
-            <li key={index}>
-              <img src={imageItem.url} alt={`Photo ${index}`} />
-            </li>
-          ))
-        }
-      </ul>
-    </div>
+      <div className={styles.imgArea}>
+        <ul>
+          {
+            img.image_list.map((imageItem, index) => (
+              <li key={index}>
+                <img src={imageItem.url} alt={`Photo ${index}`} />
+              </li>
+            ))
+          }
+        </ul>
+      </div>
+    </>
   );
 }
 
