6303952368990ee662776d0c7e96f2389e7534c4
‘Taichi-Sakai’ <130420550+sakai-taichi@users.noreply.github.com>
2024-04-15 23:51:47 +0900

    ファイル文言修正

---
+++ src/routers/photo/Picture.jsx
@@ -0,0 +1,19 @@
+import { useParams } from 'react-router-dom';
+import Header from '../../components/common/Header'
+import PicMain from '../../components/PicMain'
+import Footer from '../../components/common/Footer'
+
+function Picture ( {link} ) {
+
+  const { id } = useParams(); // URLからidパラメータを取得
+
+  return (
+    <div>
+      <Header urlCheck={link} />
+      <PicMain id={id} />
+      <Footer />
+    </div>
+  )
+}
+
+export default Picture;
