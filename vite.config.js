2b5ef99167fc8227edc674a2a12bcb5403bcc8d4
taichisakai <sakai111893@gmail.com>
2024-05-10 00:44:18 +0900

    公開サーバーとローカルサーバーで区別する

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,6 +7,9 @@ export default defineConfig({
   server: {
     historyApiFallback: {
       rewrites: [{ from: /^\/*/, to: '/index.html' }],
-    }
+    },
+  },
+  define: {
+    'import.meta.env': process.env.NODE_ENV === 'production' ? {} : import.meta.env
   }
-})
+});
