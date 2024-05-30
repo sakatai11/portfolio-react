32b7e98c84b13d2f046c197f5a36c4740a85a7b9
taichisakai <sakai111893@gmail.com>
2024-05-30 21:22:54 +0900

    本番環境へチェックアウト

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,15 @@
+// 本番環境ではconsole.logを無効にする
+if (process.env.NODE_ENV === 'production') {
+  console.log = function() {};
+}
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  
   <React.StrictMode>
     <BrowserRouter>
       <App />
