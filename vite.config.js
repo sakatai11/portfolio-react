c13661b2ececde96744a32c4ce0aa11ca61b462e
taichisakai <sakai111893@gmail.com>
2024-05-31 00:56:42 +0900

    terser のインストール

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import { terser } from 'rollup-plugin-terser';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    // 本番環境でのみterserを適用する
+    process.env.NODE_ENV === 'production' && terser({
+      compress: {
+        drop_console: true, // 本番ビルドからconsoleを削除
+        drop_debugger: true // 本番ビルドからdebuggerを削除
+      }
+    })
+  ],
   server: {
     historyApiFallback: {
       rewrites: [{ from: /^\/*/, to: '/index.html' }],
@@ -13,12 +23,5 @@ export default defineConfig({
   define: {
     'import.meta.env': process.env.NODE_ENV === 'production' ? {} : import.meta.env
   },
-  build: {
-    minify: 'esbuild', // esbuildを使用してminifyする
-    esbuild: {
-      minify: process.env.NODE_ENV === 'production',
-      drop: ['console', 'debugger'], // 本番ビルドからconsoleとdebuggerを削除
-    }
-  }
-
+  // esbuildの設定は削除
 });
