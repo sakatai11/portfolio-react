4bff422a9fac897d9c5b3022ab740e05a0336c28
taichisakai <sakai111893@gmail.com>
2024-06-02 16:34:16 +0900

    react-helmetとTOPの修正

--- src/routers/top/Top.jsx
+++ src/routers/top/Top.jsx
@@ -2,10 +2,18 @@ import Animation from "../../components/layouts/Animation";
 import Main from "../../components/Main";
 import styles from "../../components/layouts/Top.module.css";
 import PropTypes from "prop-types";
+import { Helmet } from "react-helmet";
 
 function Top({ link }) {
 	return (
 		<>
+		  <Helmet>
+        <title>トップページ | TACHI’s PHOTO </title>
+				<meta
+					name="description"
+					content="TAICHIのポートフォリオ写真集。フィルムカメラを使って撮影するのが好きです。幅広いジャンルで撮影しています。"
+				/>
+      </Helmet>
 			<Animation>
 				<Main url={link} styles={styles.main} />
 			</Animation>
